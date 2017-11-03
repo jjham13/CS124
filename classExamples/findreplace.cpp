86118affd8f8a40559e865f1a238c2740782ad1a
Chad Macbeth <macbethc@byui.edu>
2017-11-03 16:50:11 -0600

    Nov 3 Class

---
+++ classExamples/findreplace.cpp
@@ -0,0 +1,48 @@
+/***********************************************************************
+* Program:
+*    Assignment ##, ????          (e.g. Assignment 10, Hello World)
+*    Brother {Cook, Comeau, Falin, Lilya, Honeycutt, Unsicker, Peterson, Phair, Ellsworth}, CS124
+* Author:
+*    your name
+* Summary: 
+*    Enter a brief description of your program here!  Please note that if
+*    you do not take the time to fill out this block, YOU WILL LOSE POINTS.
+*    Before you begin working, estimate the time you think it will
+*    take you to do the assignment and include it in this header block.
+*    Before you submit the assignment include the actual time it took.
+*
+*    Estimated:  0.0 hrs   
+*    Actual:     0.0 hrs
+*      Please describe briefly what was the most difficult part.
+************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+void findReplace(char text[], char find, char replace)
+{
+   int i = 0;
+   while (text[i] != 0)
+   {
+      if (text[i] == find)
+      {
+         text[i] = replace;
+      }
+      i++;
+   }
+}
+
+
+/**********************************************************************
+ * Add text here to describe what the function "main" does. Also don't forget
+ * to fill this out with meaningful text or YOU WILL LOSE POINTS.
+ ***********************************************************************/
+int main()
+{
+   char text[50] = "I will go and do!";
+
+   cout << text << endl;
+   findReplace(text,'o', '@');
+   cout << text << endl; 
+   return 0;
+}
