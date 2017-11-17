6b8182029a46110f43cd6004ec128b5b19046f76
Chad Macbeth <macbethc@byui.edu>
2017-11-17 16:00:10 -0700

    Draw Update

--- classExamples/draw.cpp
+++ classExamples/draw.cpp
@@ -5,18 +5,60 @@ using namespace std;
 
 bool readFile(int picture[][10], int size)
 {
+   int row = 0;
+   int col = 0;
+   int value = 0;
    ifstream fileIn("picture.txt");
    if (fileIn.fail())
    {
       cout << "Unable to open file." << endl;
       return false;
    }
-   // Add code
+   while (fileIn >> value)
+   {
+      if (row == size) // Exceed array
+      {
+         cout << "File too big." << endl;
+         fileIn.close();
+         return false;
+      }
+      picture[row][col] = value;
+      if (col == size - 1)  // End of row
+      {
+         col = 0;
+         row++;
+      }
+      else // Still in row
+      {
+         col++;
+      }
+   }
+   if (row != size)  // Didn't fill array
+   {
+      cout << "File too small." << endl;
+      fileIn.close();
+      return false;
+   }
+   return true; // Array is full
 }
 
 bool draw(int picture[][10], int size)
 {
-   // Add code
+   for (int row=0; row<size; row++)
+   {
+      for (int col=0; col<size; col++)
+      {
+         if (picture[row][col] == 1)
+         {
+            cout << "*";
+         }
+         else
+         {
+            cout << " ";
+         }
+      }
+      cout << endl;
+   }
 }
 
 int main()
